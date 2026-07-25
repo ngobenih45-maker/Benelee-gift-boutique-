980e61c09a6b08c0cde5130cacfea2d54a428d05
Hlaluko <ngobenih45@gmail.com>
2026-07-25 19:14:20 +0200

    Add database connection setup in db.php

---
+++ includes/db.php
@@ -0,0 +1,23 @@
+<?php
+
+$host = "localhost";
+$dbname = "benelee_gift_boutique";
+$user = "root";
+$pass = "";
+
+try {
+
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $user,
+        $pass
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e){
+
+    die("Database Connection Failed: " . $e->getMessage());
+
+}
+?>
